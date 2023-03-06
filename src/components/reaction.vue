827339126a9876a308e78bf85e80d9629326ceb4
Avoutra Randriah <randriaharimalaladjauck43@gmail.com>
2023-03-06 11:24:00 +0300

    refactor page publication & add pcomponents reaction

--- src/components/reaction.vue
+++ src/components/reaction.vue
@@ -1,7 +1,27 @@
 <template>
 <div>
-    <v-row>
-        <v-icon>fas fa-like</v-icon>
+    <v-row justify="space-between" class="my-2">
+        <v-hover>
+            <template  v-slot='{ hover }'>
+                 <v-icon :color='hover? "blue":"grey"'  @click="$emit('like')">fas fa-thumbs-up</v-icon>
+            </template>
+        </v-hover>
+        <v-hover>
+            <template v-slot='{ hover }'>
+                <v-icon :color='hover? "blue":"grey"'  @click="$emit('dislike')">fas fa-thumbs-down</v-icon>
+             </template>
+        </v-hover>
+        <v-hover>
+            <template v-slot='{ hover }'>
+                <v-icon :color='hover? "red":"grey"'   @click="$emit('love')">fas fa-heart</v-icon>
+             </template>
+        </v-hover>
+        <v-hover>
+            <template v-slot='{ hover }'>
+                <v-icon :color='hover? "green":"grey"'  @click="$emit('comment')">fas fa-comment</v-icon>
+             </template>
+        </v-hover>
+        
     </v-row>
 </div>
 </template>
--- src/views/Publication/pagePublication.vue
+++ src/views/Publication/pagePublication.vue
@@ -1,8 +1,13 @@
 <template>
  <div>
     <formulairePublier></formulairePublier>
-    <v-card class="pa-8">
-        <PublicationHeader :items="publication" ></PublicationHeader>
+    <v-card class="pa-8" v-for="item,i in publication" :key="i">
+        <PublicationHeader :items="item" ></PublicationHeader>
+        <div class="col-4 ml-16">
+            <Reaction @like="reagir('like')" @dislike="reagir('dislike')" @love="reagir('love')"
+             @comment="$router.push({path:'/commentaire',params:{id_pub: item.id_pub}})"/>
+        </div>
+        
     </v-card>
     
  </div>
@@ -10,22 +15,43 @@
 <script>
 import formulairePublier from "../../components/formulairePublier"
 import PublicationHeader from "../../components/PublicationHeader"
+import Reaction from "../../components/reaction"
 
 export default {
     name:"pagePublication",
     components:{
         formulairePublier,
-        PublicationHeader
+        PublicationHeader,
+        Reaction
     } ,
     data(){
         return{
-     publication :{
+     publication :[{
         id_pub:3,
         publication:{nom:"KENNYS",prenom:"Sedera",role:"Etudiant"},
         domaine:"Developpement mobile",matiere:["Vue.js","Node.Js","Express.js"],
         image_url:["admin.jpg","web image/background_image.jpg","web image/background_image.jpg","web image/background_image.jpg","web image/background_image.jpg","web image/background_image (6).jpg"],
         sujet:'Sujet  à resolver',created_at: '2022-07-01 15:13:44',nom:"Randriaharimalala",prenom:"Avotra",contenue: 'commenter',id_user: 1,role:"etudiant",profil_image:'admin.jpg'
-       }
+       },
+       {
+        id_pub:3,
+        publication:{nom:"KENNYS",prenom:"Sedera",role:"Etudiant"},
+        domaine:"Developpement mobile",matiere:["Vue.js","Node.Js","Express.js"],
+        image_url:["admin.jpg","web image/background_image.jpg","web image/background_image.jpg","web image/background_image.jpg","web image/background_image.jpg","web image/background_image (6).jpg"],
+        sujet:'Sujet  à resolver',created_at: '2022-07-01 15:13:44',nom:"Randriaharimalala",prenom:"Avotra",contenue: 'commenter',id_user: 1,role:"etudiant",profil_image:'admin.jpg'
+       },
+       {
+        id_pub:3,
+        publication:{nom:"KENNYS",prenom:"Sedera",role:"Etudiant"},
+        domaine:"Developpement mobile",matiere:["Vue.js","Node.Js","Express.js"],
+        image_url:["admin.jpg","web image/background_image.jpg","web image/background_image.jpg","web image/background_image.jpg","web image/background_image.jpg","web image/background_image (6).jpg"],
+        sujet:'Sujet  à resolver',created_at: '2022-07-01 15:13:44',nom:"Randriaharimalala",prenom:"Avotra",contenue: 'commenter',id_user: 1,role:"etudiant",profil_image:'admin.jpg'
+       }]
+        }
+    },
+    methods:{
+        reagir(data){
+           alert(data)
         }
     } 
 }
